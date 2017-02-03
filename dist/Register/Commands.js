1fd20780d90e08c0853c28ad9055a639073d19aa
Sawyer Hopkins <sawyer@sawyerhopkins.com>
2017-02-03 13:28:11 -0600

    dont track build files

--- dist/Register/Commands.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-exports.default = function (client) {
-    client.registry.registerGroups([['fun', 'Fun commands']]);
-};
